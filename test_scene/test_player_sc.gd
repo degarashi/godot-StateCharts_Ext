1468681cacf7394a3aefd1ec878cba665e20d99f
Degarashi <degarasu@gmail.com>
2026-05-21 22:33:47 +0900

    [update] Test Scene's scdef

--- test_scene/test_player_sc.gd
+++ test_scene/test_player_sc.gd
@@ -4,13 +4,20 @@ class_name TestPlayerSC extends StateChartExt
 
 class Event:
 	extends StateChartExt.Event
+	## Start moving the player.
+	## This triggers a transition to the Move state.
 	static var move := e()
+	## Stop moving
 	static var stop := e()
+	## Health has been updated
 	static var health_changed := e()
 
 class Param:
 	extends StateChartExt.Param
+	## Current health points
 	static var health := p(TYPE_FLOAT, {TestPlayerSC.Event.health_changed: true})
+	## Movement speed in pixels per second.
+	## Default is usually 0.0.
 	static var speed := p(TYPE_FLOAT)
 
 # [Override]
