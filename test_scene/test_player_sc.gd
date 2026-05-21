c3fa55b1571c0bdb1a063eb7a0c7de115d3be46f
Degarashi <degarasu@gmail.com>
2026-05-21 22:19:22 +0900

    [update] Test Scene's statemachine (scdef)

--- test_scene/test_player_sc.gd
+++ test_scene/test_player_sc.gd
@@ -1,7 +1,6 @@
+# [StateChartExt] Generated boilerplate. Do not edit manually.
 @tool
-class_name TestPlayerSC
-extends StateChartExt
-
+class_name TestPlayerSC extends StateChartExt
 
 class Event:
 	extends StateChartExt.Event
@@ -9,12 +8,11 @@ class Event:
 	static var stop := e()
 	static var health_changed := e()
 
-
 class Param:
 	extends StateChartExt.Param
 	static var health := p(TYPE_FLOAT, {TestPlayerSC.Event.health_changed: true})
 	static var speed := p(TYPE_FLOAT)
 
-
+# [Override]
 func get_sc_info() -> SCInfo:
 	return SCInfo.new(Param, Event)
