1f3a2afb8cedadb07886fa5adcaf3c1ec1f3ad60
Degarashi <degarasu@gmail.com>
2026-06-04 15:53:31 +0900

    [update] Make the scxmlassign action key constant and organize the code

--- addons/godot-statecharts_ext/structures/scxml_assign_action.gd
+++ addons/godot-statecharts_ext/structures/scxml_assign_action.gd
@@ -1,16 +1,21 @@
 class_name SCXMLAssignAction
 extends SCXMLAction
 
+# ------------- [Constants] -------------
+const KEY_LOCATION := "location"
+const KEY_EXPR := "expr"
+
+# ------------- [Public Variable] -------------
 var location: String
 var expr: String
 
-
+# ------------- [Callbacks] -------------
 func _init(data: Dictionary) -> void:
 	super(StateChartExt.ACTION_TYPE_ASSIGN)
-	location = data.get("location", "")
-	expr = data.get("expr", "")
+	location = data.get(KEY_LOCATION, "")
+	expr = data.get(KEY_EXPR, "")
 
-
+# ------------- [Public Method] -------------
 func execute(sc: StateChartExt) -> void:
 	if not location.is_empty():
 		sc._evaluate_and_assign(location, expr)
@@ -18,6 +23,6 @@ func execute(sc: StateChartExt) -> void:
 
 func to_dict() -> Dictionary:
 	var d := super()
-	d["location"] = location
-	d["expr"] = expr
+	d[KEY_LOCATION] = location
+	d[KEY_EXPR] = expr
 	return d
