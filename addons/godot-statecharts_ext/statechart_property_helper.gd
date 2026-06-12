867f64afc115ab4bcf737d34c33f69e98171ee44
Degarashi <degarasu@gmail.com>
2026-06-12 15:42:27 +0900

    [update] add comments

--- addons/godot-statecharts_ext/statechart_property_helper.gd
+++ addons/godot-statecharts_ext/statechart_property_helper.gd
@@ -2,12 +2,15 @@ class_name StateChartPropertyHelper
 extends RefCounted
 
 
+## StateChartExtのプロパティリストの取得
+## パラメータ、警告除外設定、履歴などのインスペクター用構築
 static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 	var properties: Array[Dictionary] = []
 	var sc_info: StateChartExt.SCInfo = sc.get_sc_info()
 	if sc_info == null:
 		return properties
 
+	# パラメータ定義を取得してインスペクターに表示する
 	var params := StateChartExt._init_and_get_entries(sc_info.param, StateChartExt.ParamEnt)
 	if not params.is_empty():
 		properties.append(
@@ -19,6 +22,7 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 			}
 		)
 
+		# 各パラメータのプロパティ定義を構築
 		for p_name in params:
 			var ent := params[p_name] as StateChartExt.ParamEnt
 			var usage := PROPERTY_USAGE_DEFAULT
@@ -35,6 +39,7 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 					]
 				))
 
+			# パラメータのプロパティ定義を追加
 			properties.append(
 				{
 					"name": StateChartConstants.PropGroup.PARAM + display_name,
@@ -43,8 +48,10 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 				}
 			)
 
+	# イベント定義を取得してインスペクターに表示する
 	var events := StateChartExt._init_and_get_entries(sc_info.event, StateChartExt.EventEnt)
 	if not events.is_empty():
+		# 未使用イベント警告除外設定グループを追加
 		properties.append(
 			{
 				"name": "Exclude Unused Warnings",
@@ -53,6 +60,7 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 				"hint_string": StateChartConstants.PropGroup.EXC_UNUSED
 			}
 		)
+		# 各イベントの除外設定プロパティを追加
 		for ev_name in events:
 			properties.append(
 				{
@@ -62,6 +70,7 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 				}
 			)
 
+		# 不明イベント警告除外設定グループを追加
 		properties.append(
 			{
 				"name": "Exclude Unknown Warnings",
@@ -70,6 +79,7 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 				"hint_string": StateChartConstants.PropGroup.EXC_UNKNOWN
 			}
 		)
+		# 各イベントの除外設定プロパティを追加
 		for ev_name in events:
 			properties.append(
 				{
@@ -79,7 +89,9 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 				}
 			)
 
+	# ランタイム履歴を表示する
 	if not sc._runtime_history.is_empty():
+		# ランタイム履歴表示グループを追加
 		properties.append(
 			{
 				"name": "Runtime History (Latest first)",
@@ -88,6 +100,7 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 				"hint_string": StateChartConstants.PropGroup.HISTORY
 			}
 		)
+		# 履歴エントリのプロパティを追加
 		for i in range(sc._runtime_history.size()):
 			properties.append(
 				{
@@ -100,6 +113,8 @@ static func sc_get_property_list(sc: StateChartExt) -> Array[Dictionary]:
 	return properties
 
 
+## プロパティのバリデーション
+## 特定のプロパティのインスペクターでの表示設定（ストレージのみにする等）の制御
 static func sc_validate_property(_sc: StateChartExt, property: Dictionary) -> void:
 	if property.name == "initial_expression_properties":
 		property.usage = PROPERTY_USAGE_STORAGE
@@ -107,26 +122,32 @@ static func sc_validate_property(_sc: StateChartExt, property: Dictionary) -> vo
 		property.usage = PROPERTY_USAGE_STORAGE
 
 
+## プロパティの値の取得
+## インスペクターからアクセスされたプロパティ名に応じた適切な値の返却
 static func sc_get_property(sc: StateChartExt, property: StringName) -> Variant:
 	if property == &"e":
 		return sc._e_dyn
 	if property == &"p":
 		return sc._p_dyn
 
+	# 未使用イベント警告除外設定の取得
 	if property.begins_with(StateChartConstants.PropGroup.EXC_UNUSED):
 		var ev_name := property.trim_prefix(StateChartConstants.PropGroup.EXC_UNUSED)
 		return ev_name in sc.exclude_unused_event
 
+	# 不明イベント警告除外設定の取得
 	if property.begins_with(StateChartConstants.PropGroup.EXC_UNKNOWN):
 		var ev_name := property.trim_prefix(StateChartConstants.PropGroup.EXC_UNKNOWN)
 		return ev_name in sc.exclude_warn_unknown_events
 
+	# ランタイム履歴の取得
 	if property.begins_with(StateChartConstants.PropGroup.HISTORY):
 		var idx := int(property.trim_prefix(StateChartConstants.PropGroup.HISTORY))
 		if idx < sc._runtime_history.size():
 			return sc._runtime_history[idx]
 		return ""
 
+	# パラメータ値の取得
 	if property.begins_with(StateChartConstants.PropGroup.PARAM):
 		var p_name := property.trim_prefix(StateChartConstants.PropGroup.PARAM)
 		# Handle local param display name
@@ -134,6 +155,7 @@ static func sc_get_property(sc: StateChartExt, property: StringName) -> Variant:
 			var parts := p_name.split(" ")
 			p_name = parts[-1]
 
+		# ステートチャート情報を取得
 		var sc_info: StateChartExt.SCInfo = sc.get_sc_info()
 		if sc_info:
 			var params := StateChartExt._init_and_get_entries(sc_info.param, StateChartExt.ParamEnt)
@@ -143,6 +165,8 @@ static func sc_get_property(sc: StateChartExt, property: StringName) -> Variant:
 	return null
 
 
+## プロパティの値の設定
+## インスペクターから変更されたプロパティ名に応じたStateChartExtの内部状態の更新
 static func sc_set_property(sc: StateChartExt, property: StringName, value: Variant) -> bool:
 	if property == &"e":
 		sc._e_dyn = value
@@ -151,22 +175,26 @@ static func sc_set_property(sc: StateChartExt, property: StringName, value: Vari
 		sc._p_dyn = value
 		return true
 
+	# 未使用イベント警告除外設定を更新
 	if property.begins_with(StateChartConstants.PropGroup.EXC_UNUSED):
 		var ev_name := StringName(property.trim_prefix(StateChartConstants.PropGroup.EXC_UNUSED))
 		sc._update_exclusion_list(sc.exclude_unused_event, ev_name, value)
 		return true
 
+	# 不明イベント警告除外設定を更新
 	if property.begins_with(StateChartConstants.PropGroup.EXC_UNKNOWN):
 		var ev_name := StringName(property.trim_prefix(StateChartConstants.PropGroup.EXC_UNKNOWN))
 		sc._update_exclusion_list(sc.exclude_warn_unknown_events, ev_name, value)
 		return true
 
+	# パラメータ値を更新
 	if property.begins_with(StateChartConstants.PropGroup.PARAM):
 		var p_name := property.trim_prefix(StateChartConstants.PropGroup.PARAM)
 		if p_name.contains(StateChartConstants.LocalParam.PREFIX):
 			var parts := p_name.split(" ")
 			p_name = parts[-1]
 
+		# ステートチャート情報を取得
 		var sc_info: StateChartExt.SCInfo = sc.get_sc_info()
 		if sc_info:
 			var params := StateChartExt._init_and_get_entries(sc_info.param, StateChartExt.ParamEnt)
